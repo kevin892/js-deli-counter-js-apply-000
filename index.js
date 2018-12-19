6aef06a187dd3b4e551f3b49b8d90e3d60c6fa4c
Kevin Ruggiero <kevinbruggiero@gmail.com>
2018-12-19 17:07:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (deliLine, name) {
   deliLine.push(name);
   //pushes name to the back of line
-  return `Welcome, ${name}. You are number ${deliLine.length} in line.`;
+  return (`Welcome, ${name}. You are number ${deliLine.length} in line.`);
 };
