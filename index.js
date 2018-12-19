5e49c04f7ec57fba4a205624eea51d963b786076
Kevin Ruggiero <kevinbruggiero@gmail.com>
2018-12-19 17:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber (line, name) {
   //pushes name to the back of line
   return (`Welcome, ${name}. You are number ${line.length} in line.`);
 };
+function nowServing(line){
+  if (line <= 0){
+    return ('There is nobody waiting to be served');
+  } else {
+    return (line[0]);
+  };
+};
