b00902d766e82475ba8f55e1dbf6132cb52b2c08
Kevin Ruggiero <kevinbruggiero@gmail.com>
2018-12-19 17:17:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (line, name) {
   line.push(name);
   //pushes name to the back of line
-  return (`Welcome, ${name}. You are number ${line.length++} in line.`);
+  return (`Welcome, ${name}. You are number ${line.length} in line.`);
 };
